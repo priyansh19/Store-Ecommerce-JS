cba7f2b753064ae9a89c826c6a540ee59cd4e49c
Priyansh Gupta <priyanshgupta@Priyanshs-Mac-mini.local>
2023-08-18 03:11:03 +0530

    Adding logout and security

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,36 @@
 import clientPromise from '@/lib/mongodb'
 import { MongoDBAdapter } from '@auth/mongodb-adapter'
-import NextAuth from 'next-auth'
+import NextAuth, { getServerSession } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
-export default NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
-    }),
-  ],
-  adapter: MongoDBAdapter(clientPromise)
-})
+const adminEmails = ['priyansh7533053411@gmail.com', 'guptapriyansh1907@gmail.com']
+
+export const authOptions = {
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET
+      }),
+    ],
+    adapter: MongoDBAdapter(clientPromise),
+    callbacks: {
+      session: ({session, token, user}) => {
+        if(adminEmails.includes(session?.user?.email)){
+          return session;
+        } else {
+          return false;
+        }
+      }
+    }
+}
+
+export default NextAuth(authOptions);
+
+export async function isAdminRequest(req, res){
+  const session = await getServerSession(req,res,authOptions);
+  if(!adminEmails.includes(session?.user?.email)){
+    res.status(401);
+    res.end();
+    throw 'you are not an admin !!'
+  }
+}
--- pages/api/upload.js
+++ pages/api/upload.js
@@ -6,6 +6,9 @@ import mime from 'mime-types';
 const bucketName = 'store-admin-bucket-01';
 
 export default async function handle(req, res) {
+    await mongooseConnect();
+    await isAdminRequest(req,res);
+
     const form = new multiparty.Form();
     const {fields,files} = await new Promise((resolve,reject) => {
         form.parse(req, (err, fields, files) => {
