e1c205186c76e1e45c00714b2b1d63caf3a39c9a
Priyansh Gupta <priyanshgupta@Priyanshs-Mac-mini.local>
2023-09-28 14:01:14 +0530

    Adding new reference for mongooseconnect and isadminrequest

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -2,6 +2,8 @@ import multiparty from 'multiparty';
 import {PutObjectCommand, S3Client} from '@aws-sdk/client-s3';
 import fs from 'fs';
 import mime from 'mime-types';
+import { mongooseConnect } from '@/lib/mongoose';
+import { isAdminRequest } from './auth/[...nextauth]';
 
 const bucketName = 'store-admin-bucket-01';
 
